116e5c22970bdaeff2c254d2dc38fe821cd7e93b
MohammedYoussefSoliman <m.youssef.soliman@gmail.com>
2024-03-28 15:02:31 +0200

    feat(configs): added import linting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,10 +6,17 @@ module.exports = {
     "plugin:react/recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:react-hooks/recommended",
+    "plugin:import/recommended",
+    "plugin:import/typescript",
   ],
   ignorePatterns: ["dist", ".eslintrc.cjs"],
   parser: "@typescript-eslint/parser",
   plugins: ["react-refresh"],
+  settings: {
+    "import/parsers": {
+      "@typescript-eslint/parser": [".ts", ".tsx"],
+    },
+  },
   rules: {
     "react-refresh/only-export-components": [
       "warn",
@@ -20,6 +27,33 @@ module.exports = {
       {
         namedComponents: "arrow-function",
         unnamedComponents: "arrow-function",
+      },
+    ],
+    "import/order": [
+      "error",
+      {
+        alphabetize: {
+          order: "asc",
+          caseInsensitive: true,
+        },
+        "newlines-between": "always",
+        groups: [
+          "builtin",
+          "external",
+          "internal",
+          "type",
+          "parent",
+          "sibling",
+          "index",
+        ],
+        pathGroups: [
+          {
+            pattern: "react",
+            group: "external",
+            position: "before",
+          },
+        ],
+        pathGroupsExcludedImportTypes: ["builtin"],
       },
     ],
     "react/react-in-jsx-scope": 0,
